c61f5f822b261dfedd2fdf69ee7ad846b80fd538
Finn Rebassoo <finn.rebassoo@cern.ch>
2017-10-23 14:35:16 -0700

    Updated

--- Ntupler/python/ConfFile_cfg.py
+++ Ntupler/python/ConfFile_cfg.py
@@ -4,16 +4,16 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = ''
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
-#process.MessageLogger.cerr.FwkReport.reportEvery = 1
+#process.MessageLogger.cerr.FwkReport.reportEvery = 500
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 #process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1001) )
 #process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(3000) )
-#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(200) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 #process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20000) )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(300) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 #process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
-#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5000) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
@@ -37,14 +37,29 @@ process.source = cms.Source("PoolSource",
         #'file:pickevents_2016B.root',
         #'file:pickevents_2016C.root',
         #'file:pickevents_2016G.root'
-        'file:readHepMC_cff_py_GEN_SIM_RECOBEFMIX_DIGI_RECO.root'
+        #'file:readHepMC_cff_py_GEN_SIM_RECOBEFMIX_DIGI_RECO.root'
+        #'file:/hadoop/cms/store/user/rebassoo/TestFiles/MuonEG-RunG-D43CFA2B-C192-E611-AC1E-0090FAA57BA0.root'
+        #'file:/hadoop/cms/store/user/rebassoo/TestFiles/WWTo2L2Nu_13TeV-powheg-2E768F23-D8B0-E611-B3D3-0242AC130004.root'
+        #'file:SUS-RunIISummer16DR80Premix-00036.root',
+        #'file:readHepMC_cff_py_GEN_SIM_RECOBEFMIX_DIGI_RECO.root',
+        #'file:/home/users/rebassoo/work/CMSSW_8_0_21/src/SUS-RunIISummer16DR80Premix-00036.root',
+        #'file:FSQ-RunIISummer16DR80Premix-00036.root'
+        #'file:/home/users/rebassoo/work/CMSSW_8_0_21/src/2017-08-10-SMExclusiveWW-25k/FSQ-RunIISummer16DR80Premix-00036.root'
+        #'file:/home/users/rebassoo/work/CMSSW_8_0_21/src/2017-08-10-SMExclusiveWW-2nd-25k/FSQ-RunIISummer16DR80Premix-00036.root'
+        #'file:/hadoop/cms/store/user/rebassoo/GammaGammaMuMuFiles/GammaGammaMuMu-Elastic-RunIIWinter15R-Summer16Moriond17-0002.root'
+        #'root://se01.indiacms.res.in//store/mc/RunIISummer16DR80Premix/WWTo2L2Nu_13TeV-powheg/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/120000/6C709675-36B2-E611-8E68-FA163E8DA59B.root',
+        #'root://se01.indiacms.res.in//store/mc/RunIISummer16DR80Premix/WWTo2L2Nu_13TeV-powheg/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/120000/746CEFB5-35B1-E611-A5B3-001E674FAF23.root',
+        #'root://se01.indiacms.res.in//store/mc/RunIISummer16DR80Premix/WWTo2L2Nu_13TeV-powheg/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/120000/7496F3A6-A8B0-E611-BA28-001E67456F68.root',
+        #'root://se01.indiacms.res.in//store/mc/RunIISummer16DR80Premix/WWTo2L2Nu_13TeV-powheg/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/120000/786ED34A-DAB0-E611-9FA4-001E674FB063.root'
+        #'file:/hadoop/cms/store/user/rebassoo/TestFiles/DoubleEG-RunB-A6C55E4E-E997-E611-A869-008CFA1111F4.root'
         #'file:/hadoop/cms/store/user/rebassoo/TestFiles/MuonEG-PromptReco-v3_0C2205B1-A29F-E611-8E3F-02163E014472.root'
         #'file:/hadoop/cms/store/user/rebassoo/TestFiles/MuonEG-RunD_2649EE95-C388-E611-AAD1-3417EBE64402.root'
+        #'file:/hadoop/cms/store/user/rebassoo/TestFiles/MuonEG-Run2016H-v3-087BD90F-6B9F-E611-912F-02163E01448A.root'
         #'file:/hadoop/cms/store/user/rebassoo/TestFiles/MuonEG-RunD-CE007154-EB88-E611-A13B-0025905C54DA.root'
-        #'file:/hadoop/cms/store/user/rebassoo/TestFiles/WWTo2L2Nu_13TeV-powheg-herwigpp_02B67C54-09B2-E611-866F-6C3BE5B51168.root'
+        'file:/hadoop/cms/store/user/rebassoo/TestFiles/WWTo2L2Nu_13TeV-powheg-herwigpp_02B67C54-09B2-E611-866F-6C3BE5B51168.root'
         #'root://cmsxrootd.fnal.gov//store/data/Run2016F/DoubleMuon/AOD/23Sep2016-v1/50000/9E0F8266-0590-E611-A14E-0242AC130002.root'
     )#,
-   #skipEvents=cms.untracked.uint32(1100)
+#                          skipEvents=cms.untracked.uint32(1247)
 )
 
 process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
@@ -65,8 +80,8 @@ else:
 #CHANNEL="mue"
 process.load("SlimmedNtuple.Ntupler.CfiFile_cfi") 
 process.demo.ismc=ISMC
-process.demo.ispps=True
-process.demo.channel="mumu"
+process.demo.ispps=False
+process.demo.channel="mue"
 
 from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
 # turn on VID producer, indicate data format  to be
