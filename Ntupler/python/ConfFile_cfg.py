c5a31a833852201ed13a58885853f189daba6f93
Finn Rebassoo <finn.rebassoo@cern.ch>
2018-02-27 09:10:29 -0800

    Updated

--- Ntupler/python/ConfFile_cfg.py
+++ Ntupler/python/ConfFile_cfg.py
@@ -7,7 +7,7 @@ process.MessageLogger.cerr.threshold = ''
 #process.MessageLogger.cerr.FwkReport.reportEvery = 500
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
 
 process.source = cms.Source("PoolSource",
@@ -17,7 +17,9 @@ process.source = cms.Source("PoolSource",
         #'root://cmsxrootd.fnal.gov//store/data/Run2016F/DoubleMuon/AOD/23Sep2016-v1/50000/9E0F8266-0590-E611-A14E-0242AC130002.root'
         #'root://cms-xrd-global.cern.ch//store/data/Run2017B/DoubleMuon/AOD/17Nov2017-v1/30000/001137DB-26D6-E711-8E99-02163E014465.root'
         #'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/AODSIM/94X_mc2017_realistic_v10-v1/00000/003FC78E-80EC-E711-8B6A-008CFAC93D9C.root'
-        'file:/hadoop/cms/store/user/rebassoo/TestFiles/DYJets2017-003FC78E-80EC-E711-8B6A-008CFAC93D9C.root'
+        #'file:/hadoop/cms/store/user/rebassoo/TestFiles/DYJets2017-003FC78E-80EC-E711-8B6A-008CFAC93D9C.root'
+        'root://cms-xrd-global.cern.ch//store/data/Run2017C/DoubleMuon/AOD/17Nov2017-v1/30000/507F651E-CCD8-E711-8022-00259020084C.root'
+        #'root://cms-xrd-global.cern.ch//store/data/Run2017F/DoubleMuon/AOD/17Nov2017-v1/70000/BAF4959F-7BDE-E711-A6CD-02163E0138BD.root'
         )#,
          #                   skipEvents=cms.untracked.uint32(169)
 )
@@ -30,8 +32,8 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('RecoMET.METFilters.badGlobalMuonTaggersAOD_cff')
 
 
-#ISMC = False
-ISMC = True
+ISMC = False
+#ISMC = True
 from Configuration.AlCa.GlobalTag import GlobalTag
 if ISMC:
     process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
@@ -41,7 +43,7 @@ else:
 
 process.load("SlimmedNtuple.Ntupler.CfiFile_cfi") 
 process.demo.ismc=ISMC
-process.demo.ispps=False
+process.demo.ispps=True
 process.demo.channel="mumu"
 
 from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
