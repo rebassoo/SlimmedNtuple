c09fb5cc207dbb281af019c91c7d92fe9b1cfcc4
Finn Rebassoo <finn.rebassoo@cern.ch>
2018-09-10 13:27:10 -0700

    Updated

--- Ntupler/python/HLTFilter_cfi.py
+++ Ntupler/python/HLTFilter_cfi.py
@@ -5,15 +5,16 @@ import copy
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 hltFilter = copy.deepcopy(hltHighLevel)
 hltFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
-hltFilter.HLTPaths = ['HLT_PFHT1050_v*',
-                      'HLT_AK8PFJet500_v*',
-                      'HLT_AK8PFJet360_TrimMass30_v*',
-                      'HLT_AK8PFJet380_TrimMass30_v*',
-                      'HLT_AK8PFJet400_TrimMass30_v*',
-                      'HLT_AK8PFJet420_TrimMass30_v*',
-                      'HLT_AK8PFHT750_TrimMass50_v*',
-                      'HLT_AK8PFHT800_TrimMass50_v*',
-                      'HLT_AK8PFHT850_TrimMass50_v*',
-                      'HLT_AK8PFHT900_TrimMass50_v*']
+hltFilter.HLTPaths = ['HLT_IsoMu27_v*']
+#hltFilter.HLTPaths = ['HLT_PFHT1050_v*',
+#                      'HLT_AK8PFJet500_v*',
+#                      'HLT_AK8PFJet360_TrimMass30_v*',
+#                      'HLT_AK8PFJet380_TrimMass30_v*',
+#                      'HLT_AK8PFJet400_TrimMass30_v*',
+#                      'HLT_AK8PFJet420_TrimMass30_v*',
+#                      'HLT_AK8PFHT750_TrimMass50_v*',
+#                      'HLT_AK8PFHT800_TrimMass50_v*',
+#                      'HLT_AK8PFHT850_TrimMass50_v*',
+#                      'HLT_AK8PFHT900_TrimMass50_v*']
 
 hltFilter.throw = cms.bool(False)
