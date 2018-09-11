75d78c692ea1b8b9b19ff01210bcc6ee83a2f0c6
Finn Rebassoo <finn.rebassoo@cern.ch>
2018-09-11 14:48:05 -0700

    Updated for global tags to redo jet energy corrections

--- Ntupler/python/ConfFile_cfg.py
+++ Ntupler/python/ConfFile_cfg.py
@@ -13,8 +13,13 @@ process.source = cms.Source("PoolSource",
         #        'file:/tmp/jjhollar/0C08DB9E-3543-E811-BB4D-0CC47A4D75F4.root'
         #        'file:/tmp/jjhollar/BulkGravToWW_narrow_M-1000_3A7945FC-1704-E811-9B66-008CFA110C5C.root'
         #        'file:/tmp/jjhollar/QCD_HT700to1000_F6F5131E-CCF9-E711-B15F-0242AC130002.root'
-        'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAOD/BulkGravToWW_narrow_M-1000_13TeV-madgraph/MINIAODSIM/PU2017_94X_mc2017_realistic_v11-v1/90000/B8CED1AB-4D46-E811-B3F9-24BE05BDAE61.root'
-    )
+        #'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAOD/BulkGravToWW_narrow_M-1000_13TeV-madgraph/MINIAODSIM/PU2017_94X_mc2017_realistic_v11-v1/90000/B8CED1AB-4D46-E811-B3F9-24BE05BDAE61.root'
+        #'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAODv2/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v2/70000/FED622C0-7975-E811-AA65-0242AC130002.root'
+        #'root://cmsxrootd.fnal.gov//store/mc/RunIIFall17MiniAODv2/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/10000/36FDBA37-1C56-E811-B75E-00266CF2E388.root'
+        #'root://xrootd.t2.ucsd.edu:2040//store/mc/RunIIFall17MiniAODv2/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/10000/B4005649-E955-E811-BE7B-0CC47A7C353E.root'
+        'root://cmsxrootd.fnal.gov//store/mc/RunIIFall17MiniAODv2/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/10000/B4005649-E955-E811-BE7B-0CC47A7C353E.root'
+        #'file:step3_fpmc_MiniAOD.root'
+        )
 )
 
 process.load("SlimmedNtuple.Ntupler.CfiFile_cfi")
@@ -22,18 +27,46 @@ process.load("SlimmedNtuple.Ntupler.CfiFile_cfi")
 process.load("SlimmedNtuple.Ntupler.HLTFilter_cfi")
 process.hltFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
 
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+MC=True
+if MC:
+    process.GlobalTag.globaltag ='94X_mc2017_realistic_v8'
+else:
+    process.GlobalTag.globaltag ='94X_dataRun2_v6'
+
+from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
+updateJetCollection(
+   process,
+   jetSource = cms.InputTag('slimmedJetsAK8'),
+   labelName = 'UpdatedJECAK8',
+   jetCorrections = ('AK8PFchs', cms.vstring(['L2Relative', 'L3Absolute','L2L3Residual']), 'NONE')  # Update: Safe to always add 'L2L3Residual' as MC contains dummy L2L3Residual corrections (always set to 1)
+)
+
+updateJetCollection(
+   process,
+   jetSource = cms.InputTag('slimmedJets'),
+   labelName = 'UpdatedJEC',
+   jetCorrections = ('AK4PFchs', cms.vstring(['L2Relative', 'L3Absolute','L2L3Residual']), 'NONE')  # Update: Safe to always add 'L2L3Residual' as MC contains dummy L2L3Residual corrections (always set to 1)
+)
+
+
 from PhysicsTools.SelectorUtils.pfJetIDSelector_cfi import pfJetIDSelector
 process.slimmedJetsAK8JetId = cms.EDFilter("PFJetIDSelectionFunctorFilter",
                                            filterParams = pfJetIDSelector.clone(),
-                                           src = cms.InputTag("slimmedJetsAK8"),
+                                           #src = cms.InputTag("slimmedJetsAK8"),
+                                           src = cms.InputTag("updatedPatJetsUpdatedJECAK8"),
                                            filter = cms.bool(True)
                                            )
 from PhysicsTools.SelectorUtils.pfJetIDSelector_cfi import pfJetIDSelector
 process.slimmedJetsJetId = cms.EDFilter("PFJetIDSelectionFunctorFilter",
                                            filterParams = pfJetIDSelector.clone(),
-                                           src = cms.InputTag("slimmedJets"),
+                                           #src = cms.InputTag("slimmedJets"),
+                                           src = cms.InputTag("updatedPatJetsUpdatedJEC"), 
                                            filter = cms.bool(True)
+
                                            )
+
 
 process.demo = cms.EDAnalyzer('Ntupler')
 # Select data or MC - this controls which jet corrections are used and whether PU reweighting info is filled                                                          
@@ -43,10 +76,13 @@ process.demo.isMC = cms.bool(True)
 process.demo.year = cms.int32(2017)
 process.demo.era = cms.string("C")
 
-process.p = cms.Path(#process.hltFilter
-                     process.slimmedJetsAK8JetId
-                     *process.slimmedJetsJetId
-                     *process.demo)
+process.p = cms.Path(process.hltFilter*
+                     process.patJetCorrFactorsUpdatedJEC * process.updatedPatJetsUpdatedJEC*
+                     process.patJetCorrFactorsUpdatedJECAK8*
+                     process.updatedPatJetsUpdatedJECAK8*
+                     process.slimmedJetsAK8JetId*
+                     process.slimmedJetsJetId*
+                     process.demo)
 
 
 
