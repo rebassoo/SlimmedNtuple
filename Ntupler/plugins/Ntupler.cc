a43d7b2bb6c5cadb9a7bf0a1188a644bdcfd7bcf
Finn Rebassoo <finn.rebassoo@cern.ch>
2018-12-17 10:38:01 -0800

    Updated

--- Ntupler/plugins/Ntupler.cc
+++ Ntupler/plugins/Ntupler.cc
@@ -12,9 +12,10 @@
 //
 Ntupler::Ntupler(const edm::ParameterSet& iConfig):
   pps_token_(consumes<std::vector<CTPPSLocalTrackLite>>(edm::InputTag(("ctppsLocalTrackLiteProducer")))),
-  gen_part_token_(consumes<reco::GenParticleCollection>(edm::InputTag("genParticles")))
+  gen_part_token_(consumes<reco::GenParticleCollection>(edm::InputTag("genParticles"))),
+  reco_protons_token_(consumes<std::vector<reco::ProtonTrack>>(edm::InputTag("ctppsProtonReconstructionOFDB")))
 {
-  
+  cout<<"I get in constructor"<<endl;
   usesResource("TFileService");
 
 }
@@ -39,6 +40,7 @@ Ntupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    using namespace edm;
    using namespace std;
 
+   cout<<"I get to beginning of code"<<endl;
 
    // Run and vertex multiplicity info
    *run_ = iEvent.id().run();
@@ -92,6 +94,60 @@ Ntupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 
 
+   // Full reco protons
+   edm::Handle<vector<reco::ProtonTrack>> recoProtons;
+   iEvent.getByToken(reco_protons_token_, recoProtons);
+       
+   // make single-RP-reco plots                                                                                                                                         
+   for (const auto & proton : *recoProtons)
+     {
+       int ismultirp = -999;
+       unsigned int decRPId = -999;
+       unsigned int armId = -999;
+       float th_y = -999;
+       float th_x = -999;
+       float t = -999;
+       float xi = -999;
+          
+       if (proton.valid())
+	 {
+	   th_y = (proton.direction().y()) / (proton.direction().mag());
+	   th_x = (proton.direction().x()) / (proton.direction().mag());
+	   xi = proton.xi();
+	          
+	   // t
+	   const double m = 0.938; // GeV                                                                                                                                               
+	   const double p = 6500.; // GeV                                                                                                                                               
+	          
+	   float t0 = 2.*m*m + 2.*p*p*(1.-xi) - 2.*sqrt( (m*m + p*p) * (m*m + p*p*(1.-xi)*(1.-xi)) );
+	   float th = sqrt(th_x * th_x + th_y * th_y);
+	   float S = sin(th/2.);
+	   t = t0 - 4. * p*p * (1.-xi) * S*S;
+	          
+	   if (proton.method == reco::ProtonTrack::rmSingleRP)
+	     {
+	       CTPPSDetId rpId(* proton.contributingRPIds.begin());
+	       decRPId = rpId.arm()*100 + rpId.station()*10 + rpId.rp();                                                                
+	       ismultirp = 0;
+	     }
+	   if (proton.method == reco::ProtonTrack::rmMultiRP)
+	     {
+	       CTPPSDetId rpId(* proton.contributingRPIds.begin());
+	       armId = rpId.arm();                                                                                                      
+	       ismultirp = 1;
+	     }
+
+	   (*proton_xi_).push_back(proton.xi());
+	   (*proton_thy_).push_back(th_y);
+	   (*proton_thx_).push_back(th_x);
+	   (*proton_t_).push_back(t);
+	   (*proton_ismultirp_).push_back(ismultirp);
+	   (*proton_rpid_).push_back(decRPId);
+	   (*proton_arm_).push_back(armId);
+	 }
+     }
+
+
 
    tree_->Fill();
 
@@ -110,6 +166,15 @@ Ntupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    (*gen_proton_xi_).clear();
    (*gen_proton_t_).clear();
 
+   (*proton_xi_).clear();
+   (*proton_thy_).clear();
+   (*proton_thx_).clear();
+   (*proton_t_).clear();
+   (*proton_ismultirp_).clear();
+   (*proton_rpid_).clear();
+   (*proton_arm_).clear();
+
+
 
 }
 
@@ -120,6 +185,7 @@ void
 Ntupler::beginJob()
 {
 
+  cout<<"I get in begin Job"<<endl;
   edm::Service<TFileService> fs; 
   tree_=fs->make<TTree>("SlimmedNtuple","SlimmedNtuple");
 
@@ -134,6 +200,15 @@ Ntupler::beginJob()
   gen_proton_xi_ = new std::vector<float>;
   gen_proton_t_ = new std::vector<float>;
 
+  proton_xi_ = new std::vector<float>;
+  proton_thy_ = new std::vector<float>;
+  proton_thx_ = new std::vector<float>;
+  proton_t_ = new std::vector<float>;
+  proton_ismultirp_ = new std::vector<int>;
+  proton_rpid_ = new std::vector<int>;
+  proton_arm_ = new std::vector<int>;
+
+
   ev_ = new long int;
   run_ = new int;
   lumiblock_ = new int;
@@ -146,10 +221,18 @@ Ntupler::beginJob()
   tree_->Branch("gen_proton_pz",&gen_proton_pz_);
   tree_->Branch("gen_proton_xi",&gen_proton_xi_);
   tree_->Branch("gen_proton_t",&gen_proton_t_);
+  tree_->Branch("proton_xi",&proton_xi_);
+  tree_->Branch("proton_thx",&proton_thx_);
+  tree_->Branch("proton_thy",&proton_thy_);
+  tree_->Branch("proton_t",&proton_t_);
+  tree_->Branch("proton_ismultirp_",&proton_ismultirp_);
+  tree_->Branch("proton_rpid",&proton_rpid_);
+  tree_->Branch("proton_arm",&proton_arm_);
   tree_->Branch("run",run_,"run/I");
   tree_->Branch("event",ev_,"event/L");
   tree_->Branch("lumiblock",lumiblock_,"lumiblock/I");
 
+  cout<<"I get to end of begin Job"<<endl;
 
 }
 
@@ -166,6 +249,16 @@ Ntupler::endJob()
   delete gen_proton_pz_;
   delete gen_proton_xi_;
   delete gen_proton_t_;
+
+  delete proton_xi_;
+  delete proton_thy_;
+  delete proton_thx_;
+  delete proton_t_;
+  delete proton_ismultirp_;
+  delete proton_rpid_;
+  delete proton_arm_;
+
+
 
 }
 
