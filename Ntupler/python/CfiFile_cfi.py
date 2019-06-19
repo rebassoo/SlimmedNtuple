83624a9c5ee51a5c7cc4c85c19955074c9239bce
Finn Rebassoo <finn.rebassoo@cern.ch>
2019-06-19 11:32:09 -0700

    Updated

--- Ntupler/python/CfiFile_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-import os
-
-TFileService = cms.Service("TFileService", fileName = cms.string("SlimmedNtuple.root") )
-
-print os.getcwd()
-
-demo = cms.EDAnalyzer('Ntupler')
